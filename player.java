536ae4d2d8f54194ae7af11b2af4c2a62849855e
gparrishcsueb <124746101+gparrishcsueb@users.noreply.github.com>
2023-03-05 18:06:31 -0800

    Update player.java
    has some errors with how the array lists are declared

--- player.java
+++ player.java
@@ -12,19 +12,25 @@ import java.util.ArrayList;
 public class player {
 
 	//integer objects are a placeholder, full version will use the card object class
-	ArrayList<Integer> handList = new ArrayList<Integer>();
+	ArrayList<Integer> handList = new ArrayList();
 	//represents the entire tableau
-	ArrayList<ArrayList<Integer>> tableauList = new ArrayList<ArrayList<Integer>>();
+	ArrayList<ArrayList<Integer>> tableauList = new ArrayList();
 	//represents each individual row
-	ArrayList<Integer> row1 = new ArrayList<Integer>();
-	ArrayList<Integer> row2 = new ArrayList<Integer>();
-	ArrayList<Integer> row3 = new ArrayList<Integer>();
-	ArrayList<Integer> row4 = new ArrayList<Integer>();
+	ArrayList<Integer> row1 = new ArrayList();
+	ArrayList<Integer> row2 = new ArrayList();
+	ArrayList<Integer> row3 = new ArrayList();
+	ArrayList<Integer> row4 = new ArrayList();
+	tableauList.add(row1);
+	tableauList.add(row2);
+	tableauList.add(row3);
+	tableauList.add(row4);
 	Integer island = null;
 	Integer climate = null;
 	int sproutCount;
 	int soil;
 	int compost;
+	int tableauTotal;
+	int score;
 	
 	//constructor that takes two arguments
 	
@@ -37,6 +43,12 @@ public class player {
 		this.climate = climate;
 	}
 	
+	//first turn that resolves island ability
+	void firstTurn() {
+		//for loop that draws the amount of cards as given by island
+		soil+=0; //placeholder. will add soil as given by island
+	}
+	
 	//draw a card and add it to hand
 	
 	void drawCard() {
@@ -44,6 +56,30 @@ public class player {
 		//TODO: Use card constructor
 		this.handList.add(newCard);
 	}
+	
+	//plant action
+	void plantAction() {
+		
+	}
+	
+	void compostAction() {
+		
+	}
+	
+	void waterAction() {
+		
+	}
+	
+	//grow action
+	void growAction() {
+		
+	}
+	
+	//compost action
+	
+	//water action
+	
+	//grow action
 	
 	//choose a card within the tableau, return card reference
 	//uses tableau position method
@@ -77,9 +113,12 @@ public class player {
 	
 	
 	//remove a card from tableau
+	void tableauRemove() {
+		
+	}
 	
-	//add a card to tableau from hand
-	void add() {
+	//add a card to tableau
+	void tableauAdd() {
 		
 	}
 	
@@ -91,16 +130,32 @@ public class player {
 	//subtract sprouts from a card
 	//requires coordinates
 	
+	//resolve abilities
+	//takes an action color as an argument, called upon when other players do their action
+	//iterates through the tableau checking if that card matches the action color
+	//asks user if they'd like to use the ability when it finds a valid card
+	void resolveAbilities(int action) {
+		//1 = plant action 2 = compost action 3 = water action 4 = grow action
+		
+	}
+	
 	//use an ability
 	//can be in either the hand or tableau, requires coordinates
 	
+	//removes card from specified coordinate, deletes it, and adds a compost point
+	void compostCard() {
+		
+	}
 	
 	//delete a card from memory
+	void deleteCard() {
+		
+	}
 	
 	//toString() method that prints out the player's entire tableau,
 	//the player's entire hand, and all of their resources
 	public String toString() {
-		
+		//for each loop
 		
 		return "placeholder";
 	}
