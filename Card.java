70da86822ea1138c9e26d2721412f66fa39b9310
Dan Chapin <dan.chapin@lcorthopedicsinnovation.com>
2023-03-05 20:49:06 -0800

    Merge with gparrish

--- Card.java
+++ Card.java
@@ -1,12 +1,24 @@
 public class Card {
     private String name;
     private String type;
+    public int sprouts;
+    public int sproutMax;
+    public int growth;
+    public int growthMax;
+
+    public Card() {
+
+    }
 
     public Card(String n, String t) {
         name = n;
         type = t;
     }
-    
+
+    void ability() {
+
+    }
+
     @Override
     public String toString() {
         return name + " " + type;
