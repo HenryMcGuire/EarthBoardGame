6def77c32479cbd620872af85b32042a05e53fef
HenryMcGuire <115193504+HenryMcGuire@users.noreply.github.com>
2023-03-05 21:25:54 -0800

    Add files via upload

--- Card.java
+++ Card.java
@@ -5,7 +5,10 @@ public class Card {
     public Card(String n, String t) { //Default constructor for Card Class.
         name = n;
         type = t;
-        if(type == "Fauna") {
+        if(type == "Flora") {
+        	FloraCard();
+        }
+        else if(type == "Fauna") {
         	FaunaCard();
         }
         else if(type == "Island") {
@@ -17,30 +20,88 @@ public class Card {
         else if(type == "Earth") {
         	EarthCard();
         }
+        else if(type == "Ecosystem") {
+        	EcosystemCard();
+        }
+        else if(type == "Event") {
+        	EventCard();
+        }
         else {
         	System.out.println("Card has an invalid type.");
         	System.out.println("Only valid card types are: ");
+        	System.out.println("Flora");
         	System.out.println("Fauna");
         	System.out.println("Island");
         	System.out.println("Climate");
         	System.out.println("Earth");
+        	System.out.println("Ecosystem");
+        	System.out.println("Event");
         }
     }
     
+    public void FloraCard() { //Creates attributes for a Flora-type card.
+    	String plant_cost;
+    	String victory_value;
+    	String habitable_elements; 
+    	String scientific_name;
+    	String growth_space;
+    	String maximum_growth_number;
+    	String canopy_completion_vp;
+    	int ability_color;
+    	String ability1;
+    	String ability2;
+    	String flavour_text;
+    }
+    
     public void FaunaCard() { //Creates attributes for a Fauna-type card.
-    	
+    	String victory_value; 
+    	String scientific_name;
+    	int ability_color;
+    	String ability;
+    	String flavour_text;
     }
     
     public void IslandCard() { //Creates attributes for a Island-type card.
-    	
+    	String victory_value; 
+    	String scientific_name;
+    	int ability_color;
+    	String ability;
+    	String flavour_text;
     }
     
     public void ClimateCard() { //Creates attributes for a Climate-type card.
-    	
+    	String victory_value; 
+    	String scientific_name;
+    	int ability_color;
+    	String ability;
+    	String flavour_text;
     }
     
     public void EarthCard() { //Creates attributes for a Earth-type card.
-    	
+    	String plant_cost;
+    	String victory_value;
+    	String habitable_elements; 
+    	String scientific_name;
+    	int ability_color;
+    	String ability1;
+    	String ability2;
+    	String flavour_text;
+    }
+    
+    public void EcosystemCard() { //Creates attributes for a Ecosystem-type card.
+    	String victory_value; 
+    	String scientific_name;
+    	int ability_color;
+    	String ability;
+    	String flavour_text;
+    }
+    
+    public void EventCard() { //Creates attributes for a Event-type card.
+    	String victory_value; 
+    	String scientific_name;
+    	int ability_color;
+    	String ability;
+    	String flavour_text;
     }
     
     @Override
